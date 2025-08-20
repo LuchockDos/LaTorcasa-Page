669fce492d6e40b2b155c0b679c3a40777fc1f9a
LuchockDos <luciano123benitez@gmail.com>
2025-08-20 14:51:36 -0300

    LaTorcazaFinal

--- script.js
+++ script.js
@@ -4,29 +4,42 @@ const closeBtn = document.getElementById('close')
 
 document.querySelectorAll('.img_caza').forEach(img =>{
     img.addEventListener('click', () =>{
-        modal.style.display = "block";
-        modalImg.src = img.src;
+        modal.style.display = 'block'
+        modalImg.src = img.src
     })
 })
 closeBtn.addEventListener('click', () => {
-  modal.style.display = "none";
+  modal.style.display = 'none'
 })
 
 modal.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none'
   }
 })
 let lastScrollTop = 0;
-const navbarMovile = document.querySelector(".container_header_movile");
+const navbarMovile = document.getElementById('container_header_movile')
+const menuCheckbox = document.getElementById('label_icon')
 
-window.addEventListener("scroll", () => {
-  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (currentScroll > lastScrollTop) {
-    navbarMovile.style.top = "-200px"; 
+menuCheckbox.addEventListener('change', () => {
+  if (menuCheckbox.checked) {
+    document.body.style.overflow = 'hidden'
   } else {
-    navbarMovile.style.top = "0";
+    document.body.style.overflow = 'auto'
+  }
+});
+
+window.addEventListener('scroll', () => {
+  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  if (!menuCheckbox.checked) {
+    if (currentScroll > lastScrollTop) {
+      navbarMovile.style.top = '-200px'
+    } else {
+      navbarMovile.style.top = '0'
+    }
   }
   lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-});
+})
+
+
+
